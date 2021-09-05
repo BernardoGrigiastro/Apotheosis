58611329cb1d55ad452dd4ff03a6e68528ea427b
Andrew6rant <57331134+Andrew6rant@users.noreply.github.com>
2021-09-05 15:15:58 -0400

    Fabric environment setup

---
+++ src/main/java/io/github/Andrew6rant/apotheosis/Apotheosis.java
@@ -0,0 +1,9 @@
+package io.github.Andrew6rant.apotheosis;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Apotheosis implements ModInitializer {
+	@Override
+	public void onInitialize() {
+	}
+}
